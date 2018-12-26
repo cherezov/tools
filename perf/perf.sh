c80affd4f1e3117bd5339b54bd14f28ce8e9bba1
Pavel Cherezov <pavel@macbook.local>
2018-12-26 22:58:53 +0300

    perf shell script update

--- perf/perf.sh
+++ perf/perf.sh
@@ -1,10 +1,18 @@
 #! /bin/sh
 # perf.sh <command with args>
+rm -rf ./perf_data
+rm -rf ./mem.svg
+rm -rf ./cpu.svg
+
 sar -o ./perf_data -A 1 300 > /dev/null &
 pid=$!
 
+sleep 2
 time $@
-kill -9 $pid
+sleep 2
+
+kill -9 $pid 2> /dev/null
+wait $pid 2> /dev/null
 
 sadf -g ./perf_data  -- -r  > mem.svg
 sadf -g ./perf_data  -- -P ALL  > cpu.svg
